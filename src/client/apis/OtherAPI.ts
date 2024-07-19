287f31409f47220331103b1677a8e086dba1bda4
Carbuino <19510288+Carbuino@users.noreply.github.com>
2024-07-18 21:28:23 -0600

    Janky replacement for API
    Bloxy broke due a Roblox API change so hopefully this restore some functionality even if it is infinitely worse now.

--- src/client/apis/OtherAPI.ts
+++ src/client/apis/OtherAPI.ts
@@ -2,7 +2,6 @@ import BaseAPI from "./BaseAPI";
 import Client from "../Client";
 import { EnumUserPresence } from "../../interfaces/GeneralInterfaces";
 
-
 export type GetUserProfileHeaderOptions = {
     userId: number;
 }
@@ -55,13 +54,88 @@ export default class OtherAPI extends BaseAPI {
     }
 
     getUserProfileHeader (options: GetUserProfileHeaderOptions): Promise<GetUserProfileHeader> {
-        return this.request({
-            requiresAuth: false,
-            request: {
-                url: `https://www.roblox.com/users/profile/profileheader-json?userId=${options.userId}`
+        let baseData = {
+            UserId: 0,
+            ProfileUserId: 0,
+            ProfileUserName: "",
+            ProfileDisplayName: "",
+            FriendsCount: 0,
+            UserPresenceType: 0,
+            LastLocation: "",
+            UserStatus: null,
+            UserStatusDate: null,
+            UserPlaceId: null,
+            FollowersCount: 0,
+            FollowingsCount: 0,
+            IsVieweeBlocked: false,
+            IsViewerBlocked: false,
+            AreFriends: false,
+            IncomingFriendRequestPending: false,
+            MaySendFriendInvitation: false,
+            FriendRequestPending: false,
+            MayFollow: false,
+            IsFollowing: false,
+            CanMessage: true,
+            MessagesDisabled: false,
+            CanBeFollowed: false,
+            CanTrade: false,
+            CanSeeFavorites: true,
+            MayImpersonate: false,
+            MayEdit: false,
+            HeadShotImage: {
+              Final: true,
+              Url: "",
+              RetryUrl: null,
+              UserId: 0,
+              EndpointType: "Avatar"
             },
-            json: true
-        })
-            .then(response => response.body);
+            PreviousUserNames: "",
+            IsUserOnPhone: false,
+            CanSeeInventory: true
+          }
+        const userData = new Promise<GetUserProfileHeader>((resolve, reject) => {
+            try {
+                // User Data
+                this.request({requiresAuth: false, request: {url: `https://users.roblox.com/v1/users/${options.userId}`},  json: true}).then(response => {
+                    let data = response.body
+                    baseData.ProfileUserId = data.id
+                    baseData.ProfileUserName = data.name
+                    baseData.ProfileDisplayName = data.displayName
+                });
+                // User Friend Count
+                this.request({requiresAuth: false, request: {url: `https://friends.roblox.com/v1/users/${options.userId}/friends/count`},  json: true}).then(response => {
+                    let data = response.body
+                    baseData.FriendsCount = data.count
+                });
+                // User Followings Count
+                this.request({requiresAuth: false, request: {url: `https://friends.roblox.com/v1/users/${options.userId}/followings/count`},  json: true}).then(response => {
+                    let data = response.body
+                    baseData.FollowingsCount = data.count
+                });
+                // User Followers Count
+                this.request({requiresAuth: false, request: {url: `https://friends.roblox.com/v1/users/${options.userId}/followers/count`},  json: true}).then(response => {
+                    let data = response.body
+                    baseData.FollowersCount = data.count
+                });
+                // User Presense Info
+                this.request({requiresAuth: false, request: {url: `https://presence.roblox.com/v1/presence/users`, method: 'POST', body: `{"userIds": [${options.userId}]}`},  json: true}).then(response => {
+                    let data = response.body.userPresences[0]
+                    baseData.UserPresenceType = data.userPrecenceType
+                    baseData.LastLocation = data.lastLocation
+                    baseData.UserPlaceId = data.placeId
+                });
+                // User Headshot Info
+                this.request({requiresAuth: false, request: {url: `https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${options.userId}&size=420x420&format=Png&isCircular=false`},  json: true}).then(response => {
+                    let data = response.body.data[0]
+                    baseData.HeadShotImage.Url = data.imageUrl
+                    baseData.HeadShotImage.UserId = data.targetId
+                });
+                resolve(baseData)
+            } catch (error) {
+                reject(error)
+            }
+        }) 
+
+        return userData
     }
 }
